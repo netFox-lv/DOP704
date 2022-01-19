6ad47eb34d9be9fed7962b3f7fa8dcb4a4065492
netFox <68239792+netFoxLatvia@users.noreply.github.com>
2022-01-19 16:15:19 +0200

    Add files via upload

--- redis-configuration.js
+++ redis-configuration.js
@@ -1,7 +1,6 @@
 const redis = require('redis');
 const redisClient = redis.createClient({
-    host: '192.168.0.192',
-    port: 6379
+    url: 'redis://192.168.0.192:6379'
 });
 
 module.exports = redisClient;
