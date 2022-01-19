7b643c74364e49769a2c7ea7ddd6f22f0a84623c
netFox <68239792+netFoxLatvia@users.noreply.github.com>
2022-01-19 16:46:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const moment = require('moment');
 
 const redis = require('redis');
 const redisClient = redis.createClient({
-    host: '192.168.0.192'
+    url: 'redis://192.168.0.192:6379'
 });
 
 
