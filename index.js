aefe741ccd59c5514fd8c775dc065dc8a17a82ca
netFox <68239792+netFoxLatvia@users.noreply.github.com>
2022-01-22 16:35:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const exec = util.promisify(require('child_process').exec);
 const moment = require('moment');
 
 const { MongoClient } = require("mongodb");
-const uri ="mongodb+srv://localhost/?maxPoolSize=20&w=majority";
+const uri ="mongodb+srv://127.0.0.1/?maxPoolSize=20&w=majority";
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
