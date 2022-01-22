e6ef62ec3e0593dd1e8f5729888b8a5ef11e98e2
netFox <68239792+netFoxLatvia@users.noreply.github.com>
2022-01-22 22:50:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ async function setCar(plate_number){
 
             const time_result = await collection.findOne({car_plate: plate_number})
                 if (result) {
-                    console.log(result[1]);
+                    console.log(JSON.parse(result));
                 }
 
           let testAccount = await nodemailer.createTestAccount();
