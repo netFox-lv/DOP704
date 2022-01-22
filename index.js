b51f9c10eb7865351fcf96750090aa9126500364
netFox <68239792+netFoxLatvia@users.noreply.github.com>
2022-01-22 23:48:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function setCar(plate_number){
                 if (time_result) {
                     var startDate = moment(time_result.time_in);
                     var endDate = moment(time_result.time_out);
-                    var secondsDiff = endDate.diff(startDate,'minutes');
+                    var secondsDiff = endDate.diff(startDate,'seconds');
                 }
                 console.log(secondsDiff);
           let testAccount = await nodemailer.createTestAccount();
